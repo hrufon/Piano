9f84613040c09f3f0dd9762bb36858085e161b0b
Hrufon <hrufon@gmail.com>
2019-03-21 21:16:02 +0200

    first commit

---
+++ piano.js
@@ -0,0 +1,47 @@
+let A = document.getElementById("piano1"); 
+                            
+function playAudioA() { 
+A.play();
+} 
+
+let S = document.getElementById("piano2"); 
+                            
+function playAudioS() { 
+S.play();
+}
+
+let D = document.getElementById("piano3"); 
+                            
+function playAudioD() { 
+D.play();
+}
+
+let F = document.getElementById("piano4"); 
+                            
+function playAudioF() { 
+F.play();
+}
+
+let G = document.getElementById("piano5"); 
+                            
+function playAudioG() { 
+G.play();
+}
+
+let H = document.getElementById("piano6"); 
+                            
+function playAudioH() { 
+H.play();
+}
+
+let J = document.getElementById("piano7"); 
+                            
+function playAudioJ() { 
+J.play();
+}
+
+let K = document.getElementById("piano8"); 
+                            
+function playAudioK() { 
+K.play();
+}
